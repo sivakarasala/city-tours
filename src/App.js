7485ece8aad333a9a2e676874e3e8c581feb251e
shambho <Sivakrishna.Karasala@eku.edu>
2019-03-21 19:05:02 -0400

    add navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import "./App.scss";
+import Navbar from "./components/Navbar/Navbar";
 
 class App extends Component {
   render() {
-    return <h1>City tours app</h1>;
+    return (
+      <React.Fragment>
+        <Navbar />
+      </React.Fragment>
+    );
   }
 }
 
